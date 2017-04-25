b32ccae49eda3beeeb158418c39c6f584c79d0a1
Sanma <LTHNhung0626@gmail.com>
2017-04-25 22:31:58 +0700

    Initial commit

---
+++ src/TestWork.java
@@ -0,0 +1,13 @@
+/**
+ * Created by saury on 25/04/2017.
+ */
+public class TestWork
+{
+    public static void main() {
+        System.out.println("Hello");
+        // this is gonna be tough
+        for (int i = 0; i < 10; i++) {
+            System.out.println("GO FCK YOURSELF");
+        }
+    }
+}
